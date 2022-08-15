280a026a8826c9faf56211f2e84726e6b91c3010
Admin <mpas14142@gmail.com>
2022-08-15 22:03:23 +0300

    fix job

--- app/src/main/java/fast/parse/past/MainActivity.kt
+++ app/src/main/java/fast/parse/past/MainActivity.kt
@@ -1,6 +1,7 @@
 package fast.parse.past
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.*
@@ -8,7 +9,9 @@ import org.jsoup.Jsoup
 
 class MainActivity : AppCompatActivity() {
 
-
+    private val url = "https://en.wikipedia.org/"
+    private val cssQuery = "#mp-dyk > ul > li:nth-child(1)"
+    private val imgQuery = "#mp-dyk > a"
 
     @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,20 +20,25 @@ class MainActivity : AppCompatActivity() {
 
         val tv = findViewById<TextView>(R.id.tv)
         tv.textSize = 22F
+
         runBlocking {
             val job: Job = GlobalScope.launch(Dispatchers.IO) {
-                val str = webData()
-                tv.append(str)
+                tv.text = webData()
+
             }
             job.join()
+            job.cancel()
         }
     }
 
     private fun webData(): String {
-        val doc = Jsoup.connect("https://en.wikipedia.org/").get()
-        val elements = doc.select("#mp-dyk > ul > li:nth-child(1)")
+        val doc = Jsoup.connect(url).get()
+        val elements = doc.select(cssQuery)
+        val imageUrl = url + "wiki/" + doc.select(imgQuery).text()//elements.select("a").text().replace(" ", "_")
+        Log.d("************ = ", imageUrl)
 
         return Jsoup.parse(elements.toString()).wholeText().substring(8)
     }
+
 
 }
